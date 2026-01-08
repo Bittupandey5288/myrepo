aff006bb24bbdb5f60fe459aa96d546463741c0d
Bittupandey5288 <kumarbittu2720@gmail.com>
2026-01-08 22:14:57 +0530

    finalcommit

--- demo.ts
+++ demo.ts
@@ -1,7 +1,8 @@
 function my()
 {
-    console.log('hello i am type script !');
+    console.log('hello i am type script *');
     console.log('hello i am  type script v2 ');
     console.log('hello i am type script v3');
+    console.log('Hello i am final version ');
 }
 my(); // calling of the function
